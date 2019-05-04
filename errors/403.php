3d6ba3cad8e673ec2c5122e98e14dd21c9eb9986
ArminCoderVerst <44770691+ArminCoderVerst@users.noreply.github.com>
2019-05-04 20:56:42 +0300

    создание файлов 403 и 404

---
+++ errors/403.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Errors 403 Template | Kinotavr.by</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	
+	<style>
+		@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700,900);
+		
+		body{font-family: Rubik,sans-serif;margin: 0;padding: 0;background: rgba(192, 26, 26, 1.0);background: -webkit-radial-gradient(center,rgba(192, 26, 26, 1.0), rgba(254, 4, 1, 1.0));background: -moz-radial-gradient(center, rgba(192, 26, 26, 1.0), rgba(254,4, 1, 1.0));background: radial-gradient(ellipse at center, rgba(192, 26, 26, 1.0), rgba(254, 4, 1, 1.0));}
+		#wrapper{height: 100vh;width: 100vw;}
+		.error-box{width:100%;left:0;right:0;}
+		.error-body h1{font-size: 210px;font-weight: 900;}
+		.text-muted{color: white;}
+		.footer{display: block;width: 100%;}
+		.home{border-radius: 20px;opacity: 0.9;}
+		.select{color: black;}
+		@media only screen and (max-width: 520px){.error-body h1{padding-top: 25%;font-size: 110px;font-weight: 700;line-height: 110px;}}
+	</style>
+</head>
+
+<body>
+	<section id="wrapper" class="container-fluid">
+		<div class="error-box">
+			<div class="error-body text-center pt-5">
+				<h1 class="text-break">403</h1>
+				<h3 class="text-light p-2">Страница не найдена !</h3>
+				<p class="mt-3 mb-3 text-light">Ошибка дириктори, либо файл удален...</p>
+			</div>
+		</div>
+		<footer class="footer text-center text-light container-fluid py-4 mt-5"><?php echo date("Y");?> <a href="../html/Index.php" target="_blank" class="select">Kinotavr.by</a></footer>
+	</section>
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ errors/404.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Errors 404 Template | Kinotavr.by</title>
+    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    
+    <style>
+		@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700,900);
+		
+		body{font-family: Rubik,sans-serif;margin: 0;padding: 0;background-color: #27272a;}
+		#wrapper{height: 100vh;width: 100vw;}
+		.error-box{width:100%;left:0;right:0;}
+		.error-body h1{font-size: 210px;font-weight: 900;}
+		.text-danger{color: #f33155;}
+		.text-muted{color: #8d9ea7;}
+		.footer{display: block;width: 100%;}
+		.home{border-radius: 20px;opacity: 0.9;}
+		@media only screen and (max-width: 520px){.error-body h1{padding-top: 25%;font-size: 110px;font-weight: 700;line-height: 110px;}}
+	</style>
+</head>
+<body>
+	<section id="wrapper" class="container-fluid">
+		<div class="error-box">
+			<div class="error-body text-center pt-5">
+				<h1 class="text-danger">404</h1>
+				<h3 class="text-light">Страница не найдена !</h3>
+				<p class="text-muted mt-3 mb-3">Сайт который вы ищите</p>
+				<a href="../html/Index.php" class="btn btn-danger home mb-5">Назад домой =)</a>
+			</div>
+		</div>
+		<footer class="footer text-center text-light container-fluid py-4 mt-5"><?php echo date("Y");?> <a href="../html/Index.php" target="_blank">Kinotavr.by</a></footer>
+	</section>
+	
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
